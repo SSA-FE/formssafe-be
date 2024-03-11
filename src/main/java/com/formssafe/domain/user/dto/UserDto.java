91e2413e539be32726f2fd94791fe330ec5cb920
Hyeokjin Kim <80228712+gurwls0122@users.noreply.github.com>
2024-03-11 14:56:27 +0900

    fix : UserDto 변수명 변경

--- src/main/java/com/formssafe/domain/user/dto/UserDto.java
+++ src/main/java/com/formssafe/domain/user/dto/UserDto.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 public record UserDto (
         @Schema(description = "사용자 ID") int userId,
-        @Schema(description = "사용자 이름") String username,
+        @Schema(description = "사용자 별명") String nickname,
         @Schema(description = "이미지 URL") String imageUrl,
         @Schema(description = "이메일 주소") String email
 ){}
