5b663ddb3d585ac6ea8d9e107bbe67a82a50f9b0
gurwls0122 <80228712+gurwls0122@users.noreply.github.com>
2024-03-12 20:14:12 +0900

    feature : activity 구조 생성 및 swagger 작성

---
+++ src/main/java/com/formssafe/domain/activity/dto/SelfSubmissionResponse.java
@@ -0,0 +1,12 @@
+package com.formssafe.domain.activity.dto;
+
+import com.formssafe.domain.submission.dto.Response;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.util.List;
+
+public record SelfSubmissionResponse (
+        @Schema(description = "설문 폼 id") int formId,
+        @Schema(description = "응답 리스트") List<Response> responses,
+        @Schema(description = "임시 저장 여부") boolean isTemp
+){}
