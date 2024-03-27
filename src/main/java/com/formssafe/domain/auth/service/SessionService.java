e865532fd4b46fb60f739de3ca4d5f3b7e3f3e1a
minnim1010 <pms1010123@daum.net>
2024-03-27 10:01:26 +0900

    Revert "feat: 로그인/로그아웃 시 쿠키 관련 처리 추가"
    This reverts commit 6b1764d6574c2168d87f6af9ca01a82ae28e924b.

--- src/main/java/com/formssafe/domain/auth/service/SessionService.java
+++ src/main/java/com/formssafe/domain/auth/service/SessionService.java
@@ -2,9 +2,7 @@ package com.formssafe.domain.auth.service;
 
 import com.formssafe.domain.user.entity.User;
 import com.formssafe.global.exception.type.SessionNotFoundException;
-import com.formssafe.global.util.CookieUtil;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,7 +13,7 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class SessionService {
 
-    public void createSession(HttpServletRequest request, HttpServletResponse response, User user) {
+    public void createSession(HttpServletRequest request, User user) {
         HttpSession session = request.getSession(false);
         if (session != null) {
             log.debug("old session: {}", session.getId());
@@ -25,18 +23,14 @@ public class SessionService {
         session = request.getSession();
         session.setAttribute("userId", user.id());
         log.debug("new session: {} {} {}", session.getId(), user.id(), user.nickname());
-
-        CookieUtil.addCookie(response, "SESSION_ID", session.getId(), "api.formssafe.com");
     }
 
-    public void deleteSession(HttpServletRequest request, HttpServletResponse response) {
+    public void deleteSession(HttpServletRequest request) {
         HttpSession session = request.getSession(false);
         if (session == null) {
             throw new SessionNotFoundException("Session cannot be null");
         }
         session.invalidate();
         log.debug("Session invalidated.");
-
-        CookieUtil.deleteCookie(request, response, "SESSION_ID");
     }
 }
