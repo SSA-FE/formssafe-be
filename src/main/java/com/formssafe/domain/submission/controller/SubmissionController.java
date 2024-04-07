282f74c2e4eef87e24d03b3975ea5758044e5add
gurwls0122 <80228712+gurwls0122@users.noreply.github.com>
2024-04-07 22:18:21 +0900

    refactor : 설문 제출 시 제출 한 user 정보 받아오는 logic 변경

--- src/main/java/com/formssafe/domain/submission/controller/SubmissionController.java
+++ src/main/java/com/formssafe/domain/submission/controller/SubmissionController.java
@@ -3,7 +3,7 @@ package com.formssafe.domain.submission.controller;
 import com.formssafe.domain.auth.service.SessionService;
 import com.formssafe.domain.submission.dto.SubmissionRequest.SubmissionCreateDto;
 import com.formssafe.domain.submission.service.SubmissionService;
-import com.formssafe.domain.user.dto.UserRequest;
+import com.formssafe.domain.user.dto.UserRequest.LoginUserDto;
 import com.formssafe.global.exception.response.ExceptionResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -15,43 +15,53 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/forms")
 @RequiredArgsConstructor
 @Slf4j
-@Tag(name="submission", description = "설문 참여 및 수정")
+@Tag(name = "submission", description = "설문 참여 및 수정")
 public class SubmissionController {
     private final SessionService sessionService;
     private final SubmissionService submissionService;
-    @Operation(summary="설문 참여하기", description="처음으로 설문에 참여할 때 사용")
+
+    @Operation(summary = "설문 참여하기", description = "처음으로 설문에 참여할 때 사용")
     @ApiResponse(responseCode = "200", description = "설문 응답 완료")
     @ApiResponse(responseCode = "400", description = "설문이 존재하지 않거나 만료되었을 때",
-    content = @Content(mediaType = "application/json",
-            schema = @Schema(implementation = ExceptionResponse.class),
-            examples = @ExampleObject(value = "{\"error\": \"설문이 존재하지 않거나 만료되었습니다.\"}")))
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ExceptionResponse.class),
+                    examples = @ExampleObject(value = "{\"error\": \"설문이 존재하지 않거나 만료되었습니다.\"}")))
     @ApiResponse(responseCode = "401", description = "세션이 존재하지 않는 경우",
-    content= @Content(mediaType = "application/json",
-            schema = @Schema(implementation = ExceptionResponse.class),
-            examples = @ExampleObject(value = "{\"error\": \"세션이 존재하지 않습니다.\"}")))
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = ExceptionResponse.class),
+                    examples = @ExampleObject(value = "{\"error\": \"세션이 존재하지 않습니다.\"}")))
     @PostMapping("/{formId}/submission")
     @ResponseStatus(HttpStatus.OK)
-    public void createSubmission(@PathVariable long formId, @RequestBody SubmissionCreateDto request, @AuthenticationPrincipal UserRequest.LoginUserDto loginUser){
+    public void createSubmission(@PathVariable long formId, @RequestBody SubmissionCreateDto request,
+                                 @AuthenticationPrincipal LoginUserDto loginUser) {
         submissionService.create(formId, request, loginUser);
     }
-    @Operation(summary="설문 참여하기(수정)", description="임시 조정된 설문 참여")
+
+    @Operation(summary = "설문 참여하기(수정)", description = "임시 조정된 설문 참여")
     @ApiResponse(responseCode = "200", description = "설문 수정 완료")
     @ApiResponse(responseCode = "400", description = "설문이 존재하지 않거나 만료되었을 때",
             content = @Content(mediaType = "application/json",
                     schema = @Schema(implementation = ExceptionResponse.class),
                     examples = @ExampleObject(value = "{\"error\": \"설문이 존재하지 않거나 만료되었습니다.\"}")))
     @ApiResponse(responseCode = "401", description = "세션이 존재하지 않는 경우",
-            content= @Content(mediaType = "application/json",
+            content = @Content(mediaType = "application/json",
                     schema = @Schema(implementation = ExceptionResponse.class),
                     examples = @ExampleObject(value = "{\"error\": \"세션이 존재하지 않습니다.\"}")))
     @PutMapping("/{formId}/submission")
-    public void modifySubmission(@PathVariable String formId, @RequestBody SubmissionCreateDto submissionRequest){
+    public void modifySubmission(@PathVariable String formId, @RequestBody SubmissionCreateDto submissionRequest,
+                                 @AuthenticationPrincipal LoginUserDto loginUser) {
         return;
     }
 }
--- src/main/java/com/formssafe/domain/submission/service/SubmissionService.java
+++ src/main/java/com/formssafe/domain/submission/service/SubmissionService.java
@@ -1,12 +1,15 @@
 package com.formssafe.domain.submission.service;
 
-import com.formssafe.domain.content.decoration.entity.DecorationType;
-import com.formssafe.domain.content.question.entity.*;
+import com.formssafe.domain.content.question.entity.DescriptiveQuestion;
+import com.formssafe.domain.content.question.entity.DescriptiveQuestionType;
+import com.formssafe.domain.content.question.entity.ObjectiveQuestion;
+import com.formssafe.domain.content.question.entity.ObjectiveQuestionType;
 import com.formssafe.domain.content.question.service.DescriptiveQuestionService;
 import com.formssafe.domain.content.question.service.ObjectiveQuestionService;
 import com.formssafe.domain.form.entity.Form;
 import com.formssafe.domain.form.service.FormService;
-import com.formssafe.domain.submission.dto.SubmissionRequest.*;
+import com.formssafe.domain.submission.dto.SubmissionRequest.SubmissionCreateDto;
+import com.formssafe.domain.submission.dto.SubmissionRequest.SubmissionDetailDto;
 import com.formssafe.domain.submission.entity.DescriptiveSubmission;
 import com.formssafe.domain.submission.entity.ObjectiveSubmission;
 import com.formssafe.domain.submission.entity.Submission;
@@ -16,14 +19,12 @@ import com.formssafe.domain.submission.repository.SubmissionRepository;
 import com.formssafe.domain.user.dto.UserRequest.LoginUserDto;
 import com.formssafe.domain.user.entity.User;
 import com.formssafe.domain.user.repository.UserRepository;
-import com.formssafe.global.exception.type.UserNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -39,34 +40,36 @@ public class SubmissionService {
     private final ObjectiveSubmissionRepository objectiveSubmissionRepository;
 
     @Transactional
-    public void create(long formId, SubmissionCreateDto request, LoginUserDto loginUser){
-        User user = userRepository.findById(loginUser.id())
-                .orElseThrow(() -> new UserNotFoundException("유저가 존재하지 않습니다.: " + loginUser.id()));
+    public void create(long formId, SubmissionCreateDto request, LoginUserDto loginUser) {
+        User user = userRepository.getReferenceById(loginUser.id());
 
         Form form = formService.getForm(formId);
         log.info(form.toString());
+        
         Submission submission = createSubmission(request, user, form);
         log.info(submission.toString());
 
         createDetailSubmission(request.submissions(), submission);
     }
 
-    private Submission createSubmission(SubmissionCreateDto request, User user, Form form){
+    private Submission createSubmission(SubmissionCreateDto request, User user, Form form) {
         Submission submission = request.toSubmission(user, form);
         submission = submissionRepository.save(submission);
         return submission;
     }
-    private void createDetailSubmission(List<SubmissionDetailDto> submissionDetailDtos, Submission submission){
+
+    private void createDetailSubmission(List<SubmissionDetailDto> submissionDetailDtos, Submission submission) {
         List<ObjectiveSubmission> objectiveSubmissions = new ArrayList<>();
         List<DescriptiveSubmission> descriptiveSubmissions = new ArrayList<>();
 
-        for(SubmissionDetailDto dtos: submissionDetailDtos){
-            if(ObjectiveQuestionType.exists(dtos.type())){
-                ObjectiveQuestion objectiveQuestion = objectiveQuestionService.getObjectiveQuestionByUuid(dtos.questionId());
+        for (SubmissionDetailDto dtos : submissionDetailDtos) {
+            if (ObjectiveQuestionType.exists(dtos.type())) {
+                ObjectiveQuestion objectiveQuestion = objectiveQuestionService.getObjectiveQuestionByUuid(
+                        dtos.questionId());
                 objectiveSubmissions.add(dtos.toObjectiveSubmission(submission, objectiveQuestion));
-            }
-            else if(DescriptiveQuestionType.exists(dtos.type())){
-                DescriptiveQuestion descriptiveQuestion = descriptiveQuestionService.getDescriptiveQuestionByUuid(dtos.questionId());
+            } else if (DescriptiveQuestionType.exists(dtos.type())) {
+                DescriptiveQuestion descriptiveQuestion = descriptiveQuestionService.getDescriptiveQuestionByUuid(
+                        dtos.questionId());
                 descriptiveSubmissions.add(dtos.toDescriptiveSubmission(submission, descriptiveQuestion));
             }
 //            else{
