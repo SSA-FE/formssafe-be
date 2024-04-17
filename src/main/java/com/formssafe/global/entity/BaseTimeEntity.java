719ed21d3a928f4a7092d515a5c7b04cda3cbbef
minnim1010 <pms1010123@daum.net>
2024-04-17 14:12:15 +0900

    fix: 엑셀 다운로드 시 파일명이 설정되지 않는 버그 해결

--- src/main/java/com/formssafe/global/entity/BaseTimeEntity.java
+++ src/main/java/com/formssafe/global/entity/BaseTimeEntity.java
@@ -4,24 +4,20 @@ package com.formssafe.global.entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+@Getter
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
+@EqualsAndHashCode
 public abstract class BaseTimeEntity {
     @CreatedDate
     private LocalDateTime createDate;
 
     @LastModifiedDate
     private LocalDateTime modifyDate;
-
-    public LocalDateTime getCreateDate() {
-        return createDate;
-    }
-
-    public LocalDateTime getModifyDate() {
-        return modifyDate;
-    }
 }
